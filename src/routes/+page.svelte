7d595564e3cbaf783b1c3cd800ff3b91826d1ce1
charlie <charlie@maddex.dev>
2024-02-17 22:00:49 +0000

    initial refactor

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<hr />
+
+<p>
+	<b>cunjur</b>
+	<br />
+	security + research
+</p>
+
+<p>slowly learning elixir & rust.</p>
+
+<hr />
+
+<p>
+	[ <a href="https://twitter.com/cunjur" target="_blank">Twitter</a> ] [
+	<a href="https://github.com/name" target="_blank">GitHub</a> ] [
+	<a href="mailto:charlie@maddex.dev">Email</a> ] [
+	<a href="/pgp.pub" target="_blank">PGP</a> ]
+</p>
+
+<hr />
+
+<p>
+	To contact me quickly, <a href="https://twitter.com/cunjur" target="_blank">DM me on Twitter</a>.
+	To contact me securely, please use my PGP key.
+</p>
+
+<hr />
+
+<p>
+	If your inquiry is related to a network I manage or control, please send an email to my email
+	listed on the respective WHOIS contact.
+</p>
+<p>If it's a P1 emergency, you can email ops@multi.sh.</p>
+
+<hr />
